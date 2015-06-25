997e223595e51bcfb72074b8ab173ea6bcd5a05c
Rohan Cruickshank <metalavocado.games@gmail.com>
2015-06-25 16:39:42 +1000

    Added a map example

--- Week4/Week 4/Week 4/Week 4.cpp
+++ Week4/Week 4/Week 4/Week 4.cpp
@@ -4,7 +4,9 @@
 #include "stdafx.h"
 #include <iostream>
 #include <vector>
+#include <string>
 #include <list>
+#include <map>
 
 
 int _tmain(int argc, _TCHAR* argv[])
@@ -48,25 +50,25 @@ int _tmain(int argc, _TCHAR* argv[])
 	}
 	{	
 		// List Example
-		typedef std::list<int> IntegerVector;							//Defines a type allowing for simpler code and ease-of-use
-		typedef IntegerVector::iterator IntegerVectorIt;
+		typedef std::list<int> IntegerList;							//Defines a type allowing for simpler code and ease-of-use
+		typedef IntegerList::iterator IntegerListIt;
 
-		IntegerVector intVector;
+		IntegerList intList;
 
-		intVector.push_back(0); //adding to the vector
-		intVector.push_back(1);
-		intVector.push_back(2);
-		intVector.push_back(3);
-		intVector.push_back(2);
-		intVector.push_back(1);
+		intList.push_back(0); //adding to the vector
+		intList.push_back(1);
+		intList.push_back(2);
+		intList.push_back(3);
+		intList.push_back(2);
+		intList.push_back(1);
 
-		for (int index = 0; index < intVector.size(); ++index)
+		for (int index = 0; index < intList.size(); ++index)
 		{
 			//std::cout << intVector[index] << std::endl;				//Lists cannot refer to a specific value without iterating to it
 		}
 
-		for (IntegerVectorIt intIt = intVector.begin();					//When working with iterators, use pre-incement or pre-decrement to avoid excess memory usage
-			intIt != intVector.end();
+		for (IntegerListIt intIt = intList.begin();					//When working with iterators, use pre-incement or pre-decrement to avoid excess memory usage
+			intIt != intList.end();
 			++intIt)
 		{
 			int& intRef = *intIt;										//Dereferences the iterator and provides the actual value that is stored within the vector (in this case)
@@ -74,8 +76,8 @@ int _tmain(int argc, _TCHAR* argv[])
 			std::cout << intRef << std::endl;
 		}
 
-		for (IntegerVector::reverse_iterator intIt = intVector.rbegin();	//reverse_iterator processes the data in the reverse order. .begin and .end will become .rbegin and .rend
-			intIt != intVector.rend();
+		for (IntegerList::reverse_iterator intIt = intList.rbegin();	//reverse_iterator processes the data in the reverse order. .begin and .end will become .rbegin and .rend
+			intIt != intList.rend();
 			++intIt)
 		{
 			int& intRef = *intIt;
@@ -83,5 +85,36 @@ int _tmain(int argc, _TCHAR* argv[])
 			std::cout << intRef << std::endl;
 		}
 	}
+
+	{
+		typedef std::map <int, std::string > IntStringMap;
+
+		IntStringMap numberMap;
+
+		numberMap[3] = "Three";
+		numberMap[6] = "Six";
+		numberMap[1] = "One";
+		numberMap[4] = "Four";
+		numberMap[7] = "Seven";
+		numberMap[9] = "Nine";
+		numberMap[2] = "Two";
+
+		std::cout << numberMap[1] << std::endl;
+
+		IntStringMap::iterator findResult = numberMap.find(5);
+
+		if (findResult != numberMap.end())								//.end is any value not within the list
+		{
+			std::string& numberString = (*findResult).second;			//.second returns the value from the second column of the map, .first provides the key
+
+			std::cout << numberString << std::endl;
+		}
+
+	}
+
+
+
+
+	return 0;
 }
 
