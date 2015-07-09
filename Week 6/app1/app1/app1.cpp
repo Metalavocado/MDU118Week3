dbc64b8d162e6133c88ba34f3b90c93b68806a5a
Rohan Cruickshank <metalavocado.games@gmail.com>
2015-07-09 17:21:18 +1000

    Added call examples and a singleton

--- Week 6/app1/app1/app1.cpp
+++ Week 6/app1/app1/app1.cpp
@@ -39,10 +39,74 @@ using namespace GoodName;
 // Adds an additional name to the namespace allowing for ease-of-use
 namespace GN = GoodName;
 
+void ExampleFn_Value(std::string stringVal)
+{
 
+}
+
+void ExampleFn_Reference(std::string& stringVal)
+{
+
+}
+
+void ExampleFn_ConstReference(const std::string& stringVal)
+{
+
+}
+
+void ExampleFn_Pointer(std::string* stringVal)
+{
+
+}
+
+void ExampleFn_ConstPointer(const std::string* stringVal)
+{
+
+}
+
+/// 4 main ways to pass data
+/// by value					(int, float, double etc.)				(can't be edited)
+/// by reference				(both those above and below)			(can be edited)
+/// by const reference			(complex objects (vectors etc.) etc.)
+/// by pointer					(for non-existant objects (null or arbitrary), real-existent object)	
+/// by const pointer	
+
+//int* badPtr;				//sets the pointer to an arbitrary space in memory
+//int* badPtr = nullptr;	//sets the pointer to a known nothing
+
+
+
+////////////////////SINGLETON///////////////////// 
+//Can only create itself once//only created once//
+class ExampleSingleton
+{
+private:
+	ExampleSingleton()
+	{
+
+	}
+
+public:
+	static ExampleSingleton& Instance()
+	{
+		static ExampleSingleton instance;
+
+		return instance;
+	}	// Static applies to the function alone, not the current instance
+
+	void Function()
+	{
+
+	}
+};		
+
+#define ExampleSingletonInstance ExampleSingleton::Instance()
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	ExampleSingletonInstance.Function();
+
+
 	//GoodName::ExampleClass example;
 	//using namespace GoodName;
 	//ExampleClass example1;
